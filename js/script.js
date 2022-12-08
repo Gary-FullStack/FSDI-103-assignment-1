d6ec7e6ab0395e314195e34f5fbc45665ce40097
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2022-12-08 16:02:34 -0500

    update a bit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-//new stuff here//
+/*  
+This js mimics some of the user onboarding that I use on my Forum. This is very basic compared to
+the full server side validation logic that a real site uses.
+
+I did not code my forum, its a paid package that I've used for years. I do, however, customize 
+certain scripts, php and html to my own needs, usually with success.  Similiar to how some folks use Wordpress.*/
+
+
+
+const forumName = 'Talk and Stuff';
+console.log(forumName)
+
+let text = "Sign up now, the forum needs you!"
+alert(text)
+
+
+
+
